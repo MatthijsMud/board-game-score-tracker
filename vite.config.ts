03a3626b3e0a943aebc37c74a616bb99e66068e6
Matthijs <matthijsmud@gmail.com>
2023-06-25 14:39:49 +0200

    Updated vite config to include build information

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import ssl from "@vitejs/plugin-basic-ssl";
+import { z } from "zod";
+import { execSync } from 'child_process';
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), ssl()],
-})
+export default defineConfig(async ({ command, mode }) => {
+  const dev = mode === "development";
+  const environmentVariables=  z.object({
+    REPOSITORY_NAME: z.string().optional(),
+  });
+
+  const {
+    REPOSITORY_NAME
+  } = environmentVariables.parse(process.env);
+
+  return {
+    plugins: [
+      react(),
+      dev && ssl(),
+    ],
+    base: dev ? "./" : `/${REPOSITORY_NAME}/`,
+    define: {
+      __COMMIT_HASH__: JSON.stringify(execSync("git rev-parse --short HEAD").toString())
+    }
+  }
+});

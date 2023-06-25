5467d4676367621a6889c201df4cf1e4bbfb9f60
Matthijs <matthijsmud@gmail.com>
2023-06-25 15:14:34 +0200

    Added ability to swithc styles to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { MantineProvider } from "@mantine/core";
+import { useState } from "react";
+import { MantineProvider, ColorSchemeProvider, type ColorScheme } from "@mantine/core";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 
 function App() {
-  return <MantineProvider withNormalizeCSS withGlobalStyles theme={{ colorScheme: "dark" }}>
-    <RouterProvider router={router} />
-  </MantineProvider>
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("dark");
+  const toggleColorScheme = (value?: ColorScheme) => {
+    setColorScheme(value ?? colorScheme === "dark" ? "light" : "dark");
+  }
+  return <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+    <MantineProvider withNormalizeCSS withGlobalStyles theme={{ colorScheme }}>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  </ColorSchemeProvider>
 }
 
 export default App

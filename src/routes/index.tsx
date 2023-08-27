c07109a3507bd3fec288153036ce7bfbb1d890b1
Matthijs <matthijsmud@gmail.com>
2023-08-27 22:49:19 +0200

    Added 404 to routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,11 @@
 import { Route, createHashRouter, createRoutesFromElements, Outlet } from "react-router-dom";
 import { Container, Stack } from "@mantine/core";
+import { NotFound } from "./404";
 
 export const router = createHashRouter(createRoutesFromElements(
   <Route element={<Container size="lg" children={<Stack><Outlet /></Stack>} />}>
     <Route index lazy={() => import("./General")} />
+
+    <Route path="*" element={<NotFound />} />
   </Route>
 ));

ddca3db1b37924e2b6d81300f21d5fb449dcc10c
Matthijs <matthijsmud@gmail.com>
2023-08-27 23:08:32 +0200

    Increased size of theme picker.

--- src/components/ThemePicker.tsx
+++ src/components/ThemePicker.tsx
@@ -4,7 +4,7 @@ import { IconSun, IconMoonStars } from "@tabler/icons-react";
 
 export const ThemePicker: FC<ThemePicker.Props> = memo(({ }) => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  return <ActionIcon variant="default" c={ colorScheme === "dark" ? "yellow" : "blue" } onClick={() => toggleColorScheme()}>
+  return <ActionIcon variant="default" size="lg" c={ colorScheme === "dark" ? "yellow" : "blue" } onClick={() => toggleColorScheme()}>
     { colorScheme === "dark" ? <IconSun size="1rem" /> : <IconMoonStars size="1rem" />}
   </ActionIcon>
 });

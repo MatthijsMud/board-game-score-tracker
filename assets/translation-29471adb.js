da4fbe785096e3b9772f2308389632041316a1bf
MatthijsMud <MatthijsMud@users.noreply.github.com>
2023-09-01 15:12:39 +0000

    deploy: bbc8150ede8beaa073314214f2513a604e799e85

--- assets/translation-29471adb.js
+++
@@ -1,1 +0,0 @@
-const e={404:{title:"404 - Niet gevonden",description:"Hetgeen dat je zoekt kon niet worden gevonden"},title:"Score bijhouder"},t={app:e};export{e as app,t as default};
---
+++ assets/translation-b1546401.js
@@ -0,0 +1,1 @@
+const e={404:{title:"404 - Niet gevonden",description:"Hetgeen dat je zoekt kon niet worden gevonden"},title:"Score bijhouder",players:{add:"Voeg een speler toe"}},t={app:e};export{e as app,t as default};

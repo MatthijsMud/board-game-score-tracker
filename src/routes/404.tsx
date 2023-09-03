dddf9b66f85664b30c89064bfbf66200fbf46507
Matthijs <matthijsmud@gmail.com>
2023-09-03 13:06:48 +0200

    Made 404's title smaller

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -1,13 +1,13 @@
 import { type FC } from "react";
-import { Title, Text, } from "@mantine/core";
+import { Title, Text, Stack, } from "@mantine/core";
 import { useTranslation } from "react-i18next";
 
 export const NotFound: FC<NotFound.Props> = ({}) => {
   const { t } = useTranslation();
-  return <>
-    <Title>{t("app.404.title")}</Title>
+  return <Stack>
+    <Title order={2}>{t("app.404.title")}</Title>
     <Text>{t("app.404.description")}</Text>
-  </>
+  </Stack>
 }
 
 export namespace NotFound {

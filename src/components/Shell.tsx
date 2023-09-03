392142cbd6a854c833188aa38fee51d579c899de
Matthijs <matthijsmud@gmail.com>
2023-09-03 13:09:00 +0200

    Added simple back navigation

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -2,27 +2,39 @@ import { type FC, type ReactNode } from "react";
 import {
   Container,
   Group,
+  Paper,
   Stack,
   Title,
+  Anchor,
 } from "@mantine/core";
 import { useTranslation } from "react-i18next";
 import { LanguagePicker } from "./LanguagePicker";
 import { ThemePicker } from "./ThemePicker";
+import { NavLink } from "react-router-dom";
+import { IconChevronLeft } from "@tabler/icons-react";
 
 
 const Header = () => {
   const { t, ready } = useTranslation("translation", { useSuspense: false });
-  return <header>
-    <Group position="apart" noWrap>
-      <Group sx={{ overflow: "hidden"}}>
-        <Title truncate>{ready && t("app.title")}</Title>
+  return <Paper component="header" py="sm">
+    <Stack>
+      <Group position="apart" noWrap>
+        <Group sx={{ overflow: "hidden"}}>
+          <Title truncate>{ready && t("app.title")}</Title>
+        </Group>
+        <Group noWrap spacing="sm">
+          <LanguagePicker />
+          <ThemePicker />
+        </Group>
       </Group>
-      <Group noWrap spacing="sm">
-        <LanguagePicker />
-        <ThemePicker />
-      </Group>
-    </Group>
-  </header>
+      <Anchor component={NavLink} to=".." relative="path">
+        <Group>
+          <IconChevronLeft size="1em" />
+          Back
+        </Group>
+      </Anchor>
+    </Stack>
+  </Paper>
 }
 
 export const Shell: FC<Shell.Props> = ({ children }) => {

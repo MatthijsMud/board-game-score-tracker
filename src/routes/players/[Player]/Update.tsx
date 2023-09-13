6b8ad50b5d07296d6e44195532fa4aec36be7df2
Matthijs <matthijsmud@gmail.com>
2023-09-14 00:09:57 +0200

    Added page for updating player info

---
+++ src/routes/players/[Player]/Update.tsx
@@ -0,0 +1,21 @@
+import {
+  type FC,
+} from "react";
+
+export const loader = () => {
+  return null;
+}
+
+const UpdatePage: FC<UpdatePage.Props> = ({}) => {
+  return <>
+  </>
+}
+
+namespace UpdatePage {
+  export type Props = Readonly<{
+  }>;
+}
+
+export {
+  UpdatePage as Component,
+}
